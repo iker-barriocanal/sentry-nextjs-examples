121e145a7a6f9ef8e9e4654273c5d42c11f0ec59
iker-barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2021-04-29 10:05:47 +0200

    simple: Test latest beta version

--- simple/pages/api/server.js
+++ simple/pages/api/server.js
@@ -2,9 +2,9 @@
 
 import * as Sentry from '@sentry/nextjs';
 
-Sentry.init({
-  dsn: process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN,
-});
+// Sentry.init({
+//   dsn: process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN,
+// });
 
 const defaultServer = (req, res) => {
   throw new Error("sample backend error");
--- simple/pages/index.js
+++ simple/pages/index.js
@@ -5,9 +5,9 @@ import axios from "axios";
 
 import * as Sentry from '@sentry/nextjs';
 
-Sentry.init({
-  dsn: process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN,
-});
+// Sentry.init({
+//   dsn: process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN,
+// });
 
 export default function Home() {
   return (
